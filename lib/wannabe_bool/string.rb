2ba6e6380f3dcf559a1130112d3d3e90a7ccf4ca
Prodis <prodis@gmail.com>
2014-10-24 04:05:24 -0200

    Symbol.

--- lib/wannabe_bool/string.rb
+++ lib/wannabe_bool/string.rb
@@ -4,11 +4,15 @@ module WannabeBool
     TRUE_VALUES = %W{1 t true on y yes}.freeze
 
     def to_b
-      TRUE_VALUES.include?(self.strip.downcase)
+      TRUE_VALUES.include?(self.to_s.strip.downcase)
     end
   end
 end
 
 class String
+  include WannabeBool::String
+end
+
+class Symbol
   include WannabeBool::String
 end
--- spec/wannabe_bool/string_spec.rb
+++ spec/wannabe_bool/string_spec.rb
@@ -39,4 +39,43 @@ describe WannabeBool::String do
       end
     end
   end
+
+  context Symbol do
+    describe '#to_b' do
+      [ :"1", :"1 ", :" 1 ", :" 1",
+        :t, :"t ", :" t", :" t ",
+        :T, :"T ", :" T", :" T ",
+        :true, :"true ", :" true", :" true ",
+        :TRUE, :"TRUE ", :" TRUE", :" TRUE ",
+        :on, :"on ", :" on", :" on ",
+        :ON, :"ON ", :" ON ", :" ON ",
+        :y, :"y ", :" y", :" y ",
+        :Y, :"Y ", :" Y", :" Y ",
+        :yes, :"yes ", :" yes", :" yes ",
+        :YES, :"YES ", :" YES", :" YES "
+      ].each do |value|
+        context "when value is '#{value}'" do
+          subject { value }
+          it { expect(subject.to_b).to eq true }
+        end
+      end
+
+      [ :"",
+        :"0", :"2",
+        :f, :F,
+        :false,
+        :FALSE,
+        :off, :OFF,
+        :n, :N,
+        :no, :NO,
+        :not, :NOT,
+        :wherever, :Prodis
+      ].each do |value|
+        context "when value is '#{value}'" do
+          subject { value }
+          it { expect(subject.to_b).to eq false }
+        end
+      end
+    end
+  end
 end
