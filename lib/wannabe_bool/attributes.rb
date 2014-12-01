2aafb6610038c8b8d72335acf1e1bd6466ea5ed8
Prodis <prodis@gmail.com>
2014-12-01 09:46:21 -0200

    Attributes.

---
+++ lib/wannabe_bool/attributes.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+module WannabeBool
+  module Attributes
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def attr_wannabe_bool(*attributes)
+        attributes.each do |attr|
+          raise ArgumentError, "#{attr} method is not defined." unless method_defined?(attr)
+
+          define_method("#{attr}?") do
+            send(attr).to_b
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/wannabe_bool/attributes_spec.rb
@@ -0,0 +1,60 @@
+# encoding: utf-8
+class Fake
+  include WannabeBool::Attributes
+
+  attr_reader :main, :published, :migrated
+  attr_wannabe_bool :main, :published, :migrated
+
+  def initialize(main, published)
+    @main = main
+    @published = published
+  end
+
+  def migrated?
+    # Don't worry about the symbol in return, it is just to help in test expectation.
+    :original_method_return
+  end
+end
+
+describe WannabeBool::Attributes do
+  context 'when reader attribute exists' do
+    let(:subject_methods) do
+      fake = Fake.new(true, true)
+      fake.public_methods - Object.methods
+    end
+
+    it 'creates the predicate method' do
+      expect(subject_methods).to include(:main?)
+      expect(subject_methods).to include(:published?)
+    end
+
+    [ { type: 'string',  value: 'true' },
+      { type: 'integer', value: 1      },
+      { type: 'symbol',  value: :true  },
+      { type: 'boolean', value: true   }
+    ].each do |info|
+      context "with a #{info[:type]} value" do
+        subject { Fake.new(info[:value], info[:value]) }
+
+        it 'returns original value converted to boolean' do
+          expect(subject.main?).to be true
+          expect(subject.published?).to be true
+        end
+      end
+    end
+  end
+
+  context 'when reader attribute does not exist' do
+    it 'raise an ArgumentError' do
+      expect { Fake.send(:attr_wannabe_bool, :not_exist) }.to raise_error(ArgumentError, 'not_exist method is not defined.')
+    end
+  end
+
+  context 'when predicate method exists' do
+    subject { Fake.new(true, true) }
+
+    it 'does not overrides the original predicate method' do
+      expect(subject.migrated?).to eql :original_method_return
+    end
+  end
+end
